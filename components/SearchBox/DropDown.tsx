f9e0a6f809ff3b8d043f42a367332e8bc81e26ad
enkhturBaina <eegii.111.gg@gmail.com>
2024-02-25 08:08:33 +0800

    dropdown menu flex done

--- components/SearchBox/DropDown.tsx
+++ components/SearchBox/DropDown.tsx
@@ -31,63 +31,71 @@ export default function DropDown() {
           Бүгд
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="flex w-[800px] flex-row items-start bg-mainGray">
-        {state?.mainDirection &&
-          state?.mainDirection?.map((md: any, index: number) => {
-            return (
-              <div key={index}>
-                <div className="mb-5 flex flex-row">
-                  <Image
-                    src={process.env.IMG_PATH + md?.logo?.path}
-                    alt="add"
-                    height={30}
-                    width={30}
-                    className="rounded-md object-contain object-center"
-                  />
-                  <h4 className="!mt-0 ml-2 self-center text-lg font-semibold text-black">
-                    {md?.name}
-                  </h4>
+      <PopoverContent className="bg-mainGray !p-4">
+        <div className="w-[800px] columns-3 gap-y-8 space-y-8">
+          {state?.mainDirection &&
+            state?.mainDirection?.map((md: any, index: number) => {
+              return (
+                <div
+                  key={index}
+                  className="!mb-2 !mt-0 inline-block h-min w-full"
+                >
+                  <div className="mb-2 flex flex-row">
+                    <Image
+                      src={process.env.IMG_PATH + md?.logo?.path}
+                      alt="add"
+                      height={20}
+                      width={20}
+                      className="rounded-md object-contain object-center"
+                    />
+                    <h4 className="!mt-0 ml-2 self-center text-sm font-semibold text-black">
+                      {md?.name}
+                    </h4>
+                  </div>
+                  <ul>
+                    {md?.children?.map((d: any, index: number) => {
+                      return (
+                        <Popover
+                          placement="right"
+                          className="w-full"
+                          key={index}
+                        >
+                          <PopoverTrigger>
+                            <li className="mb-3 !scale-100 cursor-pointer !opacity-100 transition-all duration-300 last:mb-0 hover:text-mainColor">
+                              <div className="flex flex-row items-center justify-between">
+                                <span className="text-sm">{d.name}</span>
+                                {d.sub_children?.length !== 0 ? (
+                                  <BsChevronRight />
+                                ) : null}
+                              </div>
+                            </li>
+                          </PopoverTrigger>
+                          {d.sub_children?.length !== 0 ? (
+                            <PopoverContent className="w-40 min-w-max items-start p-4">
+                              <ul>
+                                {d.sub_children?.map(
+                                  (sub: any, index: number) => {
+                                    return (
+                                      <li
+                                        key={index}
+                                        className="mb-3 cursor-pointer transition-all duration-300 last:mb-0 hover:text-mainColor"
+                                      >
+                                        {sub.name}
+                                      </li>
+                                    );
+                                  },
+                                )}
+                              </ul>
+                            </PopoverContent>
+                          ) : null}
+                        </Popover>
+                      );
+                    })}
+                  </ul>
                 </div>
-                <ul>
-                  {md?.children?.map((d: any, index: number) => {
-                    return (
-                      <Popover placement="right" className="w-full" key={index}>
-                        <PopoverTrigger>
-                          <li className="mb-3 !scale-100 cursor-pointer !opacity-100 transition-all duration-300 last:mb-0 hover:text-mainColor">
-                            <div className="flex flex-row items-center justify-between">
-                              <span className="text-sm">{d.name}</span>
-                              {d.sub_children?.length !== 0 ? (
-                                <BsChevronRight />
-                              ) : null}
-                            </div>
-                          </li>
-                        </PopoverTrigger>
-                        {d.sub_children?.length !== 0 ? (
-                          <PopoverContent className="w-40 min-w-max items-start p-4">
-                            <ul>
-                              {d.sub_children?.map(
-                                (sub: any, index: number) => {
-                                  return (
-                                    <li
-                                      key={index}
-                                      className="mb-3 cursor-pointer transition-all duration-300 last:mb-0 hover:text-mainColor"
-                                    >
-                                      {sub.name}
-                                    </li>
-                                  );
-                                },
-                              )}
-                            </ul>
-                          </PopoverContent>
-                        ) : null}
-                      </Popover>
-                    );
-                  })}
-                </ul>
-                <Divider className="my-4" />
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </PopoverContent>
     </Popover>
   );
