edddc6046ed2d349e7143e37041145d33a2c4dc2
enkhturBaina <eegii.111.gg@gmail.com>
2024-05-07 17:10:41 +0800

    zaswar

--- pages/profile/post-service.tsx
+++ pages/profile/post-service.tsx
@@ -18,23 +18,20 @@ const PostService: NextPage = () => {
       {isAddService ? (
         <AddService isSpecial={isSpecial} setIsAddService={setIsAddService} />
       ) : (
-        <div className="flex justify-center items-start gap-6">
+        <div className="flex flex-col md:flex-row justify-center items-start gap-6">
           <div
-            className="flex h-50 p-2 flex-col justify-center items-center gap-2 rounded bg-slate-100 flex-grow flex-shrink-0"
-            onClick={() => {}}
+            className="flex h-50 md:w-1/2 w-full p-2 flex-col justify-center items-center gap-2 rounded bg-slate-100 flex-grow flex-shrink-0"
+            onClick={() => handleChoose(true)}
           >
-            <IoAddCircle
-              className="text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer"
-              onClick={() => handleChoose(true)}
-            />
-            <div className="font-medium text-center text-3xl">Онцгой үйлчилгээ</div>
+            <IoAddCircle className="text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer" />
+            <div className="font-medium text-center text-2xl">Онцгой үйлчилгээ</div>
           </div>
-          <div className="flex h-50 p-2 flex-col justify-center items-center gap-2 rounded bg-slate-100 flex-grow flex-shrink-0">
-            <IoAddCircle
-              className="text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer"
-              onClick={() => handleChoose(false)}
-            />
-            <div className="font-medium text-center text-3xl">Энгийн үйлчилгээ</div>
+          <div
+            className="flex h-50 md:w-1/2 w-full p-2 flex-col justify-center items-center gap-2 rounded bg-slate-100 flex-grow flex-shrink-0"
+            onClick={() => handleChoose(false)}
+          >
+            <IoAddCircle className="text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer" />
+            <div className="font-medium text-center text-2xl">Энгийн үйлчилгээ</div>
           </div>
         </div>
       )}
