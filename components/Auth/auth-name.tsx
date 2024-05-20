828cb12282330afbfa738689a4e1206613d21d4d
enkhturBaina <eegii.111.gg@gmail.com>
2024-05-20 20:27:37 +0800

    zaswar

--- components/Auth/auth-name.tsx
+++ components/Auth/auth-name.tsx
@@ -4,7 +4,7 @@ interface IProps {
   user: Users;
 }
 const AuthName: React.FC<IProps> = ({ user }) => (
-  <div className="ml-3 mr-5 font-bold text-black">
+  <div className="ml-3 mr-5 font-bold text-black text-lg">
     {user?.lastName != null && user?.firstName != null
       ? `${user?.lastName?.substring(0, 1)}. ${user?.firstName}`
       : 'Хэрэглэгч'}
