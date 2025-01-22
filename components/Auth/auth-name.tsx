b426fe96ab650c8c726f3b8c5f3153b280b8af9d
KaaUlziikhutag <ulziikhutag.gurensoft@gmail.com>
2025-01-22 18:27:12 +0800

    confirmation fix

--- components/Auth/auth-name.tsx
+++ components/Auth/auth-name.tsx
@@ -1,10 +1,11 @@
 import { Users } from '@/types/user';
-
+import { MdVerified } from 'react-icons/md';
 interface IProps {
   user: Users;
 }
 const AuthName: React.FC<IProps> = ({ user }) => (
-  <div className="ml-3 mr-5 font-bold text-black text-lg">
+  <div className="flex flex-row ml-3 mr-5 font-bold text-black text-lg">
+    {user.isConfirm ? <MdVerified className="mt-1 mx-1 fill-primary" /> : null}
     {user?.lastName != null && user?.firstName != null
       ? `${user?.lastName?.substring(0, 1)}. ${user?.firstName}`
       : 'Хэрэглэгч'}
--- pages/profile/confirmation.tsx
+++ pages/profile/confirmation.tsx
@@ -8,23 +8,27 @@ import { withTranslationProps } from '@/app/lib/with-translation';
 import { useRouter } from 'next/router';
 import { SsoGovService } from '@/service/sso-gov/sso-gov.service';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import { Button, Input } from '@nextui-org/react';
+import { classNames } from '@/utils/util';
 
 const Confirmation = () => {
   const router = useRouter();
-  const getToken = async (token: string) => {
-    const { response } = await SsoGovService.getToken(token);
-    if (typeof response.access_token == 'string') {
-      const data = await SsoGovService.getData(response.access_token);
-      console.log('data =========>', data);
-    }
-  };
+  const [type, setType] = useState('');
+  const [orgRegno, setOrgregno] = useState('');
 
   useEffect(() => {
-    if (typeof router.query.code == 'string') {
-      getToken(router.query.code);
-    }
+    const getToken = async (token: any) => {
+      if (typeof router.query.code == 'string') {
+        const { response } = await SsoGovService.getToken(token);
+        if (typeof response.access_token == 'string') {
+          const data = await SsoGovService.getData(response.access_token);
+          console.log('data =========>', data);
+        }
+      }
+    };
+    getToken(router.query?.code);
   }, [router.query.code]);
 
   useEffect(() => {
@@ -33,23 +37,66 @@ const Confirmation = () => {
     }
   }, [router.query.error]);
 
+  const handleConfirm = () => {
+    if (type == 'org') {
+      router.push(`${process.env.NEXT_PUBLIC_BASE_API_URL}sso-gov/auth-request-org/${orgRegno}`);
+    } else if (type == 'citizen') {
+      router.push(`${process.env.NEXT_PUBLIC_BASE_API_URL}sso-gov/auth-request-citizen`);
+    }
+  };
   return (
     <ProfileLayout>
       <div className="flex flex-col md:flex-row justify-center items-start gap-6">
-        <Link
+        <Button
           className="flex h-50 md:w-1/2 w-full p-2 flex-col justify-center items-center gap-2 rounded bg-slate-100 flex-grow flex-shrink-0"
-          href={`${process.env.NEXT_PUBLIC_BASE_API_URL}sso-gov/auth-request-org`}
+          onClick={() => setType('org')}
+          isIconOnly
         >
-          <GrOrganization className="text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer" />
+          <GrOrganization
+            className={classNames(
+              'text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer',
+              type == 'org' ? 'text-mainBlue' : null
+            )}
+          />
           <div className="font-medium text-center text-2xl">Хуулийн этгээд</div>
-        </Link>
-        <Link
+        </Button>
+        <Button
           className="flex h-50 md:w-1/2 w-full p-2 flex-col justify-center items-center gap-2 rounded bg-slate-100 flex-grow flex-shrink-0"
-          href={`${process.env.NEXT_PUBLIC_BASE_API_URL}sso-gov/auth-request-citizen`}
+          onClick={() => setType('citizen')}
+          isIconOnly
         >
-          <GrUser className="text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer" />
+          <GrUser
+            className={classNames(
+              ' text-[150px] text-mainBgGray hover:text-mainBlue cursor-pointer',
+              type == 'citizen' ? 'text-mainBlue' : null
+            )}
+          />
           <div className="font-medium text-center text-2xl">Хувь хүн</div>
-        </Link>
+        </Button>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        {type == 'org' ? (
+          <Input
+            inputMode="numeric"
+            label="Байгууллагын регистр"
+            labelPlacement="outside"
+            placeholder="--"
+            radius="sm"
+            size="lg"
+            variant="bordered"
+            classNames={{ label: 'font-bold', innerWrapper: ['custom-input-wrapper', 'bg-white'] }}
+            value={orgRegno}
+            onValueChange={setOrgregno}
+          />
+        ) : null}
+        <Button
+          className="mr-4 bg-mainColor !text-white"
+          radius="sm"
+          size="md"
+          onClick={handleConfirm}
+        >
+          Баталгаажуулах
+        </Button>
       </div>
     </ProfileLayout>
   );
